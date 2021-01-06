b978084b28e49b76e0c012262ffec478fc29d3e1
Terrance Ford <64376406+tford-dev@users.noreply.github.com>
2021-01-05 22:07:54 -0500

    Removed newPhrase
    Removed newPhrase variable from project

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -11,10 +11,8 @@ const show          = document.getElementsByClassName("show");
 const hide          = document.getElementsByClassName("hide");
 const gameMsg       = document.getElementById("game-over-message");
 let   newGame; //variable to hold Game class globally. First assigned in line 28 of app.js
-let   newPhrase; //varibale to hold Phrase class globally. First assigned in line 12 of Game.js
-let   activePhrase; //variable to hold this.phrase globally in app. First assigned in line 11 of Game.js
+let   activePhrase; //variable to hold this.phrase globally in app. First assigned in line 12 of Game.js
 let   target; //variable changes values in Game.js to assign different class names. First assigned in line 25 in Game.js
-
 //function to control logic for start game button. This button also resets. It clears "wrong" and "chosen" class from keyboard, changes png file image for hearts at the bottom of screen, creates a Game class, and calls the startGame(), keyCode(), and handleInteraction() methods from Game.js.
 const start = () =>{
     gameMsg.innerHTML += `Guess the correct phrase before the virus spreads throughout the system!`
