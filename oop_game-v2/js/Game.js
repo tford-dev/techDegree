fb9279e721f5408a09440d95b11aa29764ee8af5
Terrance Ford <64376406+tford-dev@users.noreply.github.com>
2021-01-08 20:34:14 -0500

    Removed fadeOut()
    Un-called the fadeOut() function.

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -7,7 +7,7 @@ class Game {
 
     //clears loading screen, chooses a random Phrase class, assigns active phrase to global variable, and adds active phrase to display
     startGame(){
-        fadeOut();
+        overlay.style.display = "none";
         this.activePhrase = this.getRandomPhrase(); 
         activePhrase = this.activePhrase
         console.log(activePhrase);
